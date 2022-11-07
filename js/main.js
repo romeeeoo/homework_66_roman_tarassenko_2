e9aae4834786c7076a16672b259385061af3735b
Roman Tarassenko <tarassenko@me.com>
2022-11-07 17:50:27 +0300

    Добавил main.js с решением задания №1

---
+++ js/main.js
@@ -0,0 +1,11 @@
+
+const line_1 = document.getElementById("string-3");
+console.log(line_1.innerText);
+const line_2 = document.getElementById("string-5");
+console.log(line_2.innerText);
+const line_3 = document.getElementById("string-2");
+console.log(line_3.innerText);
+const line_4 = document.getElementById("string-4");
+console.log(line_4.innerText);
+const line_5 = document.getElementById("string-1");
+console.log(line_5.innerText);
