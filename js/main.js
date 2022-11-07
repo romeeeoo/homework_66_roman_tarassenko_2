b3230b2b396fb3a428e4e83a177332e839999710
Roman Tarassenko <tarassenko@me.com>
2022-11-07 18:19:11 +0300

    Добавлено решение №2 main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-
+//task#1
 const line_1 = document.getElementById("string-3");
 console.log(line_1.innerText);
 const line_2 = document.getElementById("string-5");
@@ -9,3 +9,13 @@ const line_4 = document.getElementById("string-4");
 console.log(line_4.innerText);
 const line_5 = document.getElementById("string-1");
 console.log(line_5.innerText);
+
+
+//task#2
+const elements = document.getElementsByClassName("element")
+elements[0].style.color = "red"
+elements[1].style.color = "red"
+elements[2].style.color = "red"
+elements[3].style.color = "green"
+elements[4].style.color = "green"
+elements[5].style.color = "green"
